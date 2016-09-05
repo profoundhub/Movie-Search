b91c22fffb688cc8bc99761b69a477ac395ea164
Daniel Lim <hbt@markhamboards.com>
2016-09-05 15:54:23 -0400

    Gulp cont
    Gulp cont

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,21 @@ var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
 gulp.task('browserify'), function() {
-  browserify('src/js/main.js').transform
+  browserify('src/js/main.js').transform('reactify')
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('copy', function() {
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+  gulp.src('src/css/*.*')
+    .pipe(gulp.dest('dist'));
+  gulp.src('src/js/vendors/*.*')
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['browserify', 'copy'], function() {
+  return gulp.watch('src/**/*.*', ['browserify', 'copy']);
 });
