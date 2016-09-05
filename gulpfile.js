800b80c9641f4b8d9e48b7716d021f309d8bafec
Daniel Lim <hbt@markhamboards.com>
2016-09-05 15:55:25 -0400

    good copy
    good copy

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
-gulp.task('browserify'), function() {
+gulp.task('browserify', function() {
   browserify('src/js/main.js').transform('reactify')
     .bundle()
     .pipe(source('main.js'))
