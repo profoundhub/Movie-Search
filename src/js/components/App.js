741492fb3a3f7787ad09acdb27b8b7ded4e01c72
Daniel Lim <hbt@markhamboards.com>
2016-09-05 16:07:22 -0400

    Update Build
    Update Build

---
+++ src/js/components/App.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+var App = React.createClass({
+    render: function() {
+      return(
+        <div>
+            My App
+        </div>
+      )
+    }
+});
