398fdb3da642724b5affaa5c76aa052fb37a73ee
Daniel Lim <hbt@markhamboards.com>
2016-09-05 20:00:10 -0400

    Load Inits
    Load Inits

---
+++ src/js/dispatcher/AppDispatcher.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStores');
+
+var App = React.createClass({
+    render: function() {
+      return(
+        <div>
+            My App
+        </div>
+      )
+    }
+});
+
+module.exports = App;
---
+++ src/js/stores/AppStores.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStores');
+
+var App = React.createClass({
+  render: function() {
+    return(
+      
+    )
+  }
+});
