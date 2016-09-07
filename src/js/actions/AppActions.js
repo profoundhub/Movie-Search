d8619bd20464826711959f55dfd45eafab581390
Daniel Lim <hbt@markhamboards.com>
2016-09-07 12:58:49 -0400

    getting Fun
    getting Fun

--- src/js/actions/AppActions.js
+++ src/js/actions/AppActions.js
@@ -9,7 +9,12 @@ var AppActions = {
             actionType: AppConstants.SEARCH_MOVIES,
             movie: movie
         });
-    }
+    },
+    receiveMovieResults: function(movies) {
+        AppDispatcher.handleViewAction({
+            actionType: AppConstants.RECEIVE_MOVIE_RESULTS,
+            movies: movies
+    });
 }
 
 module.exports = AppActions;
