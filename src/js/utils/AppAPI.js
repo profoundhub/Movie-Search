b6928bf68478be6cd3f69de356db99f99fabf9a8
Daniel Lim <hbt@markhamboards.com>
2016-09-07 12:56:21 -0400

    api calls
    receiveMovieResults

--- src/js/utils/AppAPI.js
+++ src/js/utils/AppAPI.js
@@ -2,6 +2,18 @@ var AppActions = require('../actions/AppActions');
 
 module.exports = {
     searchMovies: function(movie) {
+        // ajax
+        $ajax({
+            url: 'http://www.omdbapi.com/?s=' + movie.title,
+            datatype: 'json',
+            cache: false,
+            success: function() {
+                AppActions.receiveMovieResults(data.search);
+            }.bind(this),
+            error: function(xhr, status, err) {
+                alert(err);
 
+            }.bind(this)
+        });
     }
 }
